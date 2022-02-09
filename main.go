6f490205f9b0eb5988a63f7d3f34acde54263a6d
Shawn <xiaogezz@foxmail.com>
2022-02-09 15:42:03 +0800

    refactor: 代码优化

--- main.go
+++ main.go
@@ -1,21 +1,16 @@
 package main
 
 import (
-	"database/sql"
 	"goblog/app/http/middlewares"
 	"goblog/bootstrap"
-	"goblog/pkg/database"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
 var router *mux.Router
-var db *sql.DB
 
 func main() {
-	database.Initialize()
-	db = database.DB
 
 	bootstrap.SetupDB()
 	router = bootstrap.SetupRoute()
